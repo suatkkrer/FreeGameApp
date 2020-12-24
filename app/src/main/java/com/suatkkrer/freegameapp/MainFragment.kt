4cfece59547380ff084fa28058de046fd0ef9666
suatkkrer <suat.kukrer@gmail.com>
2020-12-24 10:45:04 +0100

    Initial commit

---
+++ app/src/main/java/com/suatkkrer/freegameapp/MainFragment.kt
@@ -0,0 +1,37 @@
+package com.suatkkrer.freegameapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        fragmentButton.setOnClickListener {
+//            val action = MainFragmentDirections.actionMainFragmentToGameFragment()
+//            Navigation.findNavController(it).navigate(action)
+//        }
+        
+    }
+
+}
