3d300084f316690c1d6fb38c8901008c3f0d882d
suatkkrer <suat.kukrer@gmail.com>
2021-01-05 07:24:35 +0100

    shared preferences

--- app/src/main/java/com/suatkkrer/freegameapp/ViewModel/FeedViewModel.kt
+++ app/src/main/java/com/suatkkrer/freegameapp/ViewModel/FeedViewModel.kt
@@ -1,18 +1,24 @@
 package com.suatkkrer.freegameapp.ViewModel
 
+import android.app.Application
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.suatkkrer.freegameapp.Model.Game
 import com.suatkkrer.freegameapp.Service.GameAPIService
+import com.suatkkrer.freegameapp.Service.GameDatabase
+import com.suatkkrer.freegameapp.Util.CustomSharedPreferences
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
-class FeedViewModel : ViewModel() {
+class FeedViewModel(application: Application) : BaseViewModel(application) {
 
     private val gameAPIService = GameAPIService()
     private val disposable = CompositeDisposable()
+    private var customPreferences = CustomSharedPreferences(getApplication())
 
     val games = MutableLiveData<List<Game>>()
     val gameError = MutableLiveData<Boolean>()
@@ -31,23 +37,41 @@ class FeedViewModel : ViewModel() {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(object  : DisposableSingleObserver<List<Game>>(){
                     override fun onSuccess(t: List<Game>) {
-
-
-
-                        games.value = t
-                        gameError.value = false
-                        gameLoading.value = false
-
-
-
+                        storeInSQLite(t)
                     }
 
                     override fun onError(e: Throwable) {
                         gameError.value = true
                         gameLoading.value = false
                         e.printStackTrace()
+
                     }
                 })
         )
     }
+
+    private fun showGames(gameList: List<Game>){
+        games.value = gameList
+        gameError.value = false
+        gameLoading.value = false
+    }
+    private fun storeInSQLite(list: List<Game>){
+        launch {
+            val dao = GameDatabase(getApplication()).gameDao()
+            dao.deleteALLCountries()
+            val listLong = dao.insertAll(*list.toTypedArray())
+
+            var i = 0
+
+            while (i < list.size){
+                list[i].uuid = listLong[i].toInt()
+                i += 1
+            }
+            showGames(list)
+        }
+
+        customPreferences.saveTime(System.nanoTime())
+
+    }
+
 }
